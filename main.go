f04b9bf3c8d1a2eabf24219bfa3b3fc5448cf1b9
Lucas Baia <lucasmbaia@hotmail.com>
2024-04-02 16:07:48 -0300

    init

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run script.go <diff_file>")
+		os.Exit(1)
+	}
+
+	diffFile := os.Args[1]
+	fmt.Println("Arquivos modificados:", diffFile)
+
+	file, err := os.Open(diffFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
